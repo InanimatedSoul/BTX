4b1c9e2caf339d207d5fd932209b701075c77471
InanimatedSoul <harshitpatel163@gmail.com>
2022-06-25 14:44:33 +0530

    added comment

--- launch.bat
+++ launch.bat
@@ -2,7 +2,7 @@
 ::  this prevents the contents of the batch file from being printed to the command prompt
 @ECHO OFF
 
-:: below code executes payload.ps1 file 
+:: below code executes payload.ps1 file.
 :: start PowerShell : starts powershell in windows 
 :: -NoProfile : A potential problem that could affect PowerShell Pros is that it’s possible for them to have variables -
 ::  or other settings set in their PowerShell profile that could cause your script to not perform correctly
